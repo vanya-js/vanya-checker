3cce7925c06e1278f54fc670a0496b83e326d01b
Leonid Pospelov <pospelovlm@yandex.ru>
2022-09-05 00:11:25 +0300

    Add basic structure

---
+++ src/checks/check.js
@@ -0,0 +1,4 @@
+class Check {
+}
+
+module.exports = Check;
---
+++ src/checks/checkFileNames.js
@@ -0,0 +1,9 @@
+const Check = require('./check');
+
+class CheckFileNames extends Check {
+    constructor() {
+        super();
+    }
+}
+
+module.exports = CheckFileNames;
